44f3b018d4ca886175d306a0be6ce83a1063d427
ahamm-prequal2021 <a.hamm@codeur.online>
2021-04-13 10:49:21 +0200

    feat: back to top button

--- scripts.js
+++ scripts.js
@@ -1,25 +1,16 @@
-//Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
-    mybutton.style.display = "block";
+$(document).ready(function() {
+  $(window).scroll(function() {
+  if ($(this).scrollTop() > 20) {
+  $('#toTopBtn').fadeIn();
   } else {
-    mybutton.style.display = "none";
+  $('#toTopBtn').fadeOut();
   }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
+  });
+  
+  $('#toTopBtn').click(function() {
+  $("html, body").animate({
+  scrollTop: 0
+  }, 1000);
+  return false;
+  });
+  });
